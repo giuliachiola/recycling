dc9063c41718488c8b987aacb16b4fe23f7b27aa
Giulia Chiola <giulia.chiola@modo.md>
2018-02-24 17:00:25 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+      BFljhdsghjfkadg
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+</style>
