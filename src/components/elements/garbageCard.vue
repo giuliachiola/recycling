5ce1d07a19a0866e58e9fe6030ccb13bb4a2d016
Giulia Chiola <giulia.chiola@modo.md>
2018-02-28 22:50:01 +0100

    add colors

--- src/components/elements/garbageCard.vue
+++ src/components/elements/garbageCard.vue
@@ -1,12 +1,18 @@
 <template>
     <!-- Square card -->
     <div class="demo-card-square mdl-card mdl-shadow--2dp">
-      <div class="mdl-card__title mdl-card--expand c-card__title" :style="cardTitleStyle">
+      <div class="mdl-card__title mdl-card--expand c-card__title" :style="cardColor">
         <h2 class="mdl-card__title-text c-card__titleText">{{ garbage.name }}</h2>
         <img :src="imageSrc" class="c-card__titleImage" alt="">
       </div>
       <div class="mdl-card__supporting-text c-card__description">
-        {{ description }}
+        <p>{{ garbage.description }}</p>
+        <a
+          href="#"
+          class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored c-card__descriptionButton"
+          :style="cardColor">
+          {{ garbageBinName }}
+        </a>
       </div>
       <div class="mdl-card__actions mdl-card--border">
         <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
@@ -17,7 +23,7 @@
 </template>
 
 <script>
-import { materialColors } from '@root/data/variables.js'
+import { amber, green, indigo, cyan, brown, orange, gray, pink, red } from '@root/data/variables.js'
 
 export default {
   name: 'garbageCard',
@@ -35,28 +41,48 @@ export default {
 
   data () {
     return {
-      materialColors
+      amber, green, indigo, cyan, brown, gray, pink, red
     }
   },
 
   computed: {
-    cardTitleStyle () {
-      let binColor
-      if (this.garbage.bin === 1) {
-        binColor = 'yellow'
+    cardColor () {
+      let binColor = []
+      let randomColor
+      if (this.garbage.bin === 0) {
+        randomColor = '#000000'
+      } else {
+        if (this.garbage.bin === 1) { binColor = this.indigo } // CARTA
+        if (this.garbage.bin === 2) { binColor = this.amber } // PLASTICA
+        if (this.garbage.bin === 3) { binColor = this.green } // VETRO
+        if (this.garbage.bin === 4) { binColor = this.cyan } // ALLUMINIO
+        if (this.garbage.bin === 6) { binColor = this.brown } // ORGANICO
+        if (this.garbage.bin === 7) { binColor = this.orange } // POTATURE
+        if (this.garbage.bin === 8) { binColor = this.gray } // INDIFFERENZIATO
+        if (this.garbage.bin === 9) { binColor = this.red } // RAEE
+        if (this.garbage.bin === 10) { binColor = this.pink } // RIFIUTI SPECIALI
+        randomColor = binColor[5]
       }
-      let randomColor = this.materialColor.binColor[Math.floor(Math.random() * this.materialColor.binColor.length)]
       return { backgroundColor: randomColor }
     },
 
     imageSrc () {
-      // if (this.garbage.src) {
-      //   return require(`../../assets/img/${this.garbage.src}.png`)
-      // }
+      if (this.garbage.src) {
+        return require(`../../assets/img/${this.garbage.src}.png`)
+      }
     },
 
-    description () {
-      return this.garbage.description.slice(0, 160) + ' [...]'
+    garbageBinName () {
+      if (this.garbage.bin === 0) { return 'n/a' } // CARTA
+      if (this.garbage.bin === 1) { return 'Carta' } // CARTA
+      if (this.garbage.bin === 2) { return 'Plastica' } // PLASTICA
+      if (this.garbage.bin === 3) { return 'Vetro' } // VETRO
+      if (this.garbage.bin === 4) { return 'Alluminio' } // ALLUMINIO
+      if (this.garbage.bin === 6) { return 'Organico' } // ORGANICO
+      if (this.garbage.bin === 7) { return 'Sfalci e potature' } // POTATURE
+      if (this.garbage.bin === 8) { return 'Indifferenziato' } // INDIFFERENZIATO
+      if (this.garbage.bin === 9) { return 'RAEE' } // RAEE
+      if (this.garbage.bin === 10) { return 'Rifiuti speciali' } // RIFIUTI SPECIALI
     }
   }
 }
@@ -92,5 +118,12 @@ export default {
 
 .c-card__description {
   height: 5em;
+  position: relative;
+}
+
+.c-card__descriptionButton {
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
 }
 </style>
